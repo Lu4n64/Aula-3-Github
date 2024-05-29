3499d96c9587471b99a1f02ec4ce627e6b882e0e
Lu4n64 <luan.henrique.luz@escola.pr.gov.br>
2024-05-29 00:56:42 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+}
 
+function draw() {
+  stroke("black")
+  fill("red")
+  if(mouseIsPressed) {
+    rect(mouseX-15, mouseY-15, 30, 30);
+  }
+}
